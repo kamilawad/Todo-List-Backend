3e473a3e47c59ae6684184e600021220cd37efd0
kamilawad1 <kamilawad15@gmail.com>
2024-03-15 02:22:06 +0200

    added connection to database

---
+++ backend/connection.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "todolistdb";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
+}
